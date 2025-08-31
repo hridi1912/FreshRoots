9c38c3dc6cd8243e2093f629f0c2063f0e046281
hridi1912 <hriditaalam1@gmail.com>
2025-09-01 02:47:03 +0600

    Did dashboard for Farmer/Buyer and Signup, login page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,27 +6,19 @@ namespace FreshRoots.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        // Farmer dashboard homepage
+        public IActionResult FarmerHome()
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        public IActionResult BuyerHome()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
         }
     }
 }
