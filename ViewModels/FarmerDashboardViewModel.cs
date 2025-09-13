97eb467831d9ea15cd4ad875c91e312c9bcabe43
hridi1912 <hriditaalam1@gmail.com>
2025-09-13 09:48:39 +0600

    Completed Stats for buyer and farmer, implemented Order and status properly

---
+++ ViewModels/FarmerDashboardViewModel.cs
@@ -0,0 +1,18 @@
+﻿using FreshRoots.Models;
+
+namespace FreshRoots.ViewModels
+{
+    public class FarmerDashboardViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public DashboardStats Stats { get; set; }
+    }
+
+    public class DashboardStats
+    {
+        public int NewOrders { get; set; }
+        public int ActiveProducts { get; set; }
+        public int TotalCustomers { get; set; }
+        public decimal TotalRevenue { get; set; }
+    }
+}
